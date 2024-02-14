4a93917cb503c2533916f2866479a65a9e8df48c
sunilpalepu631 <sunil.p@orotron.com>
2024-02-14 11:59:20 +0530

    SP-ADD-nodemailer
    forget password feature by using email notification

---
+++ helpers/nodemailer.js
@@ -0,0 +1,40 @@
+
+const nodemailer = require('nodemailer')
+
+require('dotenv').config();
+
+const EMAIL_HOST_USER = process.env.EMAIL_HOST_USER
+const EMAIL_HOST_PASSWORD = process.env.EMAIL_HOST_PASSWORD
+
+sendMail = async (email, message) => {
+
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: EMAIL_HOST_USER,
+            pass: EMAIL_HOST_PASSWORD
+        }
+    });
+
+    let mailOptions = {
+        from: EMAIL_HOST_USER,
+        to: email,
+        subject: 'Forgot password',
+        text: message
+    };
+
+
+    transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+            console.log(error);
+            return error
+        } else {
+            console.log('Email sent: ' + info.response);
+            return info.response
+        }
+    });
+}
+
+
+// export default sendMail
+module.exports = sendMail
